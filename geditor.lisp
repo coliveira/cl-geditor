1bb887f72ef9963ea61233c24fb83488e2b262e0
Carlos Oliveira <coliveira@gmail.com>
2009-01-25 19:05:46 -0500

    update main file

--- geditor.lisp
+++ geditor.lisp
@@ -3,7 +3,7 @@
 (defun main ()
   (asdf:operate 'asdf:load-op 'geditor-w32))
 
-(main)
+;(main)
 
 ;; last time I tried, this didn't work (probably it is a compiler issue):
-;(sb-ext:save-lisp-and-die "geditor.exe" :toplevel 'main :executable t)
+(sb-ext:save-lisp-and-die "geditor.exe" :toplevel 'main :executable t)
