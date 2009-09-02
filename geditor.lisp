5cdc6478e5d143e87f4a134c14c23657c2a365c4
Carlos Oliveira <coliveira@gmail.com>
2009-09-02 14:29:11 -0400

    update executable so we dont need to use asdf

--- geditor.lisp
+++ geditor.lisp
@@ -1,9 +1,10 @@
-(require 'asdf)
+(make-package :geditor-w32)
 
-(defun main ()
-  (asdf:operate 'asdf:load-op 'geditor-w32))
-
-;(main)
+(defun ge-main ()
+  (load "geditor-main")
+  (in-package geditor-w32)
+  (geditor-w32::main)
+  (SB-EXT:QUIT))
 
 ;; last time I tried, this didn't work (probably it is a compiler issue):
-(sb-ext:save-lisp-and-die "geditor.exe" :toplevel 'main :executable t)
+(sb-ext:save-lisp-and-die "geditor.exe" :toplevel 'ge-main :executable t)
